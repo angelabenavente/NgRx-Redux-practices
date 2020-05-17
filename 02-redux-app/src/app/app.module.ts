5daa706dc586b0a937f535b6e3d1ab951f4a9d18
angelabenavente <angela.b.almazan@gmail.com>
2020-05-17 14:18:29 +0200

    import redux devtools

--- 02-redux-app/src/app/app.module.ts
+++ 02-redux-app/src/app/app.module.ts
@@ -1,13 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { environment } from '../environments/environment';
 
 //NgRx
 import { StoreModule } from '@ngrx/store';
+import [ StoreDevToolsModule ] from '@ngrx/store-devtools';
+
 import { counterReducer } from './counter/counter.reducer';
 
 import { AppComponent } from './app.component';
 import { ChildComponent } from './counter/child/child.component';
 import { GrandchildComponent } from './counter/grandchild/grandchild.component';
+
 
 @NgModule({
   declarations: [
@@ -17,7 +21,11 @@ import { GrandchildComponent } from './counter/grandchild/grandchild.component';
   ],
   imports: [
     BrowserModule,
-    StoreModule.forRoot({ counter: counterReducer })
+    StoreModule.forRoot({ counter: counterReducer }),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: environment.production, // Restrict extension to log-only mode
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
